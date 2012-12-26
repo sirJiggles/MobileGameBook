20fa34b95ff3bd5347f8015118503dc979437843
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-12-26 21:50:45 +0000

    more work on the first chapter of the book GF

--- Chapter-1/assets/js/engine.js
+++ Chapter-1/assets/js/engine.js
@@ -26,3 +26,69 @@ var spriteSheet = new function(){
                         s.h);
     };
 }
+
+// function for the main game
+var Game = new function(){
+    
+    // init the game 
+    this.initialize = function(canvasId, spriteData, callback){
+        
+        this.canvas = document.getElementById(canvasId);
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        
+        // setup the context
+        this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
+        
+        if(!this.ctx){
+            return alert('browser does not support canvas 2D');
+        }
+        // set up the input
+        this.setupInput();
+        
+        // start the game loop
+        this.loop();
+        
+        // load the sprite sheet and pass the callback
+        spriteSheet.load(spriteData, callback);
+    };
+    
+    // Setup the key inputs
+    var KEY_CODES = { 37:'left', 39:'right', 32:'fire'};
+    this.keys = {};
+    
+    this.setupInput = function(){
+        window.addEventListener('keydown', function(e){
+            if(KEY_CODES[event.keyCode]){
+                Game.keys[KEY_CODES[event.keyCode]] = true;
+                e.preventDefault();
+            }
+        }, false);
+        window.addEventListener('keyup', function(e){
+            if(KEY_CODES[event.keyCode]){
+                Game.keys[KEY_CODES[event.keyCode]] = false;
+                e.preventDefault();
+            }
+        }, false);
+    }
+    
+    // Game loop
+    var boards = [];
+    this.loop = function(){
+        var dt = 30 / 1000;
+        for(var i = 0, len = boards.length; i < len; i++){
+            if(boards[i]){
+                boards[i].step(dt);
+                boards[i] && boards[i].draw(Game.ctx);
+            }
+        }
+        setTimeout(Game.loop, 30);
+    };
+    
+    // change an active game board
+    this.setBoard = function(num, board){
+        boards[num] = board;
+    };
+    
+}
+
--- Chapter-1/assets/js/game.js
+++ Chapter-1/assets/js/game.js
@@ -1,21 +1,13 @@
 
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext && canvas.getContext('2d');
 
-if (!ctx){
-    alert ('unable to get context, update browser!');
-}else{
-    startGame();
+var sprites = {
+    ship: {sx:0, sy:0, w:38, h:42, frames:2}
+};
+
+var startGame = function(){
+    spriteSheet.draw(Game.ctx, 'ship', 100, 100, 0);
 }
 
-function startGame(){
-    
-    spriteSheet.load({
-        ship: {sx:0, sy:0, w:38, h:42, frames:2}
-    }, function(){
-       spriteSheet.draw(ctx, 'ship', 0, 0);
-       spriteSheet.draw(ctx, 'ship', 100, 5); 
-       spriteSheet.draw(ctx, 'ship', 150, 100); 
-    });
-}
-
+window.addEventListener('load', function(){
+    Game.initialize('canvasElement', sprites, startGame)   
+})
