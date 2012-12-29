889877eb2a378991ed241eeea9d774871f2d6e8e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-12-29 16:19:28 +0000

    got some enemies on the screen and some missiles shooting from the ship, now moving onto colision by object type

--- Alien-invasion/assets/js/engine.js
+++ Alien-invasion/assets/js/engine.js
@@ -1,5 +1,5 @@
 // function to load and draw the spritesheet
-var spriteSheet = new function(){
+var SpriteSheet = new function(){
     
     this.map = {};
     
@@ -50,11 +50,11 @@ var Game = new function(){
         this.loop();
         
         // load the sprite sheet and pass the callback
-        spriteSheet.load(spriteData, callback);
+        SpriteSheet.load(spriteData, callback);
     };
     
     // Setup the key inputs
-    var KEY_CODES = { 37:'left', 39:'right', 32:'fire', 38:'up', 40:'down' };
+    var KEY_CODES = {37:'left', 39:'right', 32:'fire', 38:'up', 40:'down'};
     this.keys = {};
     
     this.setupInput = function(){
@@ -92,6 +92,31 @@ var Game = new function(){
     
 }
 
+//Sprite function (empty because each sprite has its own constructor function)
+var Sprite = function(){}
+
+Sprite.prototype.setup = function(sprite, props){
+    this.sprite = sprite;
+    this.merge(props);
+    this.frame = this.frame || 0;
+    this.w = SpriteSheet.map[this.sprite].w;
+    this.h = SpriteSheet.map[this.sprite].h;
+}
+
+// merge method used in setup method on sprite function
+Sprite.prototype.merge = function(props){
+    if(props){
+        for(var prop in props){
+            this[prop] = props[prop];
+        }
+    }
+}
+
+// Draw meths for the sprite function
+Sprite.prototype.draw = function(ctx){
+    SpriteSheet.draw(ctx, this.sprite, this.x, this.y, this.frame);
+}
+
 //function to create the star field (scrolling background)
 var Starfield = function(speed, opacity, numStars, clear){
     //create a new canvas dom element
@@ -103,6 +128,9 @@ var Starfield = function(speed, opacity, numStars, clear){
     var starsCtx = stars.getContext('2d');
     var offset = 0;
     
+    // if the bottom level then we add black background in with the stars, if
+    // we did not do this we would still see the stars at thier previous position on draw
+    // this makes it look like we are in hyperspace!
     if(clear){
         starsCtx.fillStyle = '#000';
         starsCtx.fillRect(0, 0, stars.width, stars.height);
@@ -119,6 +147,9 @@ var Starfield = function(speed, opacity, numStars, clear){
                             2, 2);
     }
     
+    // Scrolling background draws the top row first then moves it down then
+    // then draws remaining this way when section of image is chopped of it knows 
+    // where to start from at the top creating a never ending background
     this.draw = function(ctx){
         var intOffset = Math.floor(offset);
         var remaining = stars.height - intOffset;
@@ -150,7 +181,7 @@ var TitleScreen = function(title, subtitle, callback){
         ctx.fillStyle = '#fff';
         ctx.textAlign = 'center';
         // draw the title
-        ctx.font = 'bold, 40px bangers';
+        ctx.font = 'bold 40px bangers';
         ctx.fillText(title, Game.width/2, Game.height/2);
         // draw the subtitle
         ctx.font = 'bold 20px bangers';
@@ -161,13 +192,15 @@ var TitleScreen = function(title, subtitle, callback){
 
 //function for the players ship
 var PlayerShip = function(){
-    this.w = spriteSheet.map['ship'].w;
-    this.h = spriteSheet.map['ship'].h;
+    
+    // call the setup methods in the sprite class (extended)
+    this.setup('ship', {vx:0, vy:0, frame:0, reloadTime: 0.25, maxVel: 100});
+    
     this.x = Game.width / 2 - this.w / 2;
     this.y = Game.height - 10 - this.h;
-    this.vx = 0;
-    this.vy = 0;
-    this.maxVel = 100;
+    
+    // time left before reload (set to 0.25 to prevent fire when the game starts)
+    this.reload = this.reloadTime;
     
     this.step = function(dt){
         if(Game.keys['left']){
@@ -198,10 +231,183 @@ var PlayerShip = function(){
         }else if (this.y > Game.height - this.h){
             this.y = Game.height - this.h;
         }
-    }
-    
-    this.draw = function(ctx){
-        spriteSheet.draw(ctx, 'ship', this.x, this.y, 0);
+        
+        
+        //this.reload = this.reload - dt
+        this.reload -= dt;
+        // if user clicked fire and reload time passed
+        if(Game.keys['fire'] && this.reload < 0){
+            //reset fire to false
+            Game.keys['fire'] = false;
+            //reset reload time
+            this.reload = this.reloadTime;
+            // add a couple of missiles to the board 
+            // this.board is the objctes ref to the board in this case it was given to the payer ship
+            // when it itself was added to the board and in adding the missiles to the board they to 
+            // will now have access to the board methods in this this.board property
+            this.board.add(new PlayerMissile(this.x, this.y+this.h/2));
+            this.board.add(new PlayerMissile(this.x+this.w, this.y+this.h/2));
+        }
     }
 }
 
+// Extend the sprite class here
+PlayerShip.prototype = new Sprite();
+
+
+// Game board, responsible for keeping track of objects on the board and collisions
+var GameBoard = function(){
+
+    // current objects
+    this.objects = [];
+    this.cnt = [];
+    
+    // add objects to the list funtion
+    this.add = function(obj){
+        // add a ref to the board its on, this enables us to add aditional objects like 
+        // missiles / explosions and remove itself when dead
+        obj.board = this;
+        this.objects.push(obj);
+        // init count to 0 if required using or condition, then increment by 1
+        // this is to keep track of number of objects by type on the board
+        this.cnt[obj.type] = (this.cnt[obj.type] || 0 ) +1;
+        return obj;
+    }
+    
+    // mark objects for removal function (after all objects had thier turn so not to mess
+    // up the loop)
+    this.remove = function(obj){
+        // check of the object to be removed is in the list if not add it
+        var wasStillAlive = this.removed.indexOf(obj) != -1;
+        if (wasStillAlive){
+            this.removed.push(obj);
+        }
+        return wasStillAlive;
+    }
+    
+    //Reset list of removed objects
+    this.resetRemoved = function(){this.removed = [];}
+    
+    // Remove objects marked for removal from the list
+    this.finalizeRemoved = function(){
+        // loop through the list of objects to be removed, if found splice the ojects out of
+        // the list and decrement the count of that type of object in the cnt array
+        for (var i=0, len=this.removed.length;i<len;i++){
+            var idx = this.objects.indexOf(this.removed[i]);
+            if(idx != -1){
+                this.cnt[this.removed[i].type] --;
+                this.objects.splice(idx, 1);
+            }
+        }
+    }
+    
+    // Irerate function, to run on all objects in the object list
+    this.iterate = function(funcName){
+        // get an array of rhe arguments passed to this function (value of funcName)
+        // but we want it as an array so use the prototype call to convert and splice
+        var args = Array.prototype.slice.call(arguments, 1);
+        // loop through all the objects and call the inbuilt apply function in javascript
+        for (var i=0,len=this.objects.length;i<len;i++){
+            var obj = this.objects[i];
+            // apply changes the reference to this in the function that we are calling,
+            // it is the same as call but apply requires the second arg to be an array
+            obj[funcName].apply(obj, args);
+        }
+    };
+    
+    // Detect function, find the first object for which func is returned as true
+    this.detect = function(funcName){
+        for (var i=0,val=null,len=this.objects.length;i<len;i++){
+            if(funcName.call(this.objects[i])){
+                return this.objects[i];
+            }
+        }
+        return false;
+    };
+    
+    // Step and draw functions
+    this.step = function(dt){
+        this.resetRemoved();
+        this.iterate('step', dt);
+        this.finalizeRemoved();
+    }
+    
+    this.draw = function(ctx){
+        this.iterate('draw', ctx);
+    }
+    
+    /* condition is true of objects could not collide then negate to find out if they
+     * overlap. To calculate this first check if the bottom right of 
+     * 
+     * !true if 
+     *  (A bottom is to the right of B OR A bottom is to the left of B) 
+     *      OR 
+     *  (A top to the right of B top OR A top to the left of B top)
+     */
+    this.overlap = function(o1, o2){
+        return !((o1.y+o1.h-1<o2.y) || (o1.y>o2.y+o2.h-1) || (o1.x+o1.w-1<o2.x) || (o1.x>o2.x+o2.w-1));
+    }
+    
+    // Now the function to check for collision
+    this.collide = function(obj, type){
+        // return true if this instance collides with any other obj
+        return this.detect(function(){
+            if(obj != this){
+                // collision = if (!type sent OR this.type == type (bitwise and) ) AND overlap call returns true for obj and this
+                var col = (!type || this.type & type) && board.overlap(obj, this);
+                //if colision true return this else false
+                return col ? this : false;
+            }
+        })
+    }
+};
+
+var PlayerMissile = function(x,y){
+    this.setup('missile', {vy:-700});
+    // Center the missile on x (middle)
+    this.x = x - this.w/2;
+    // y is passed in y - the height of the missle so, bottom of the missle
+    this.y = y - this.h;
+};
+
+PlayerMissile.prototype = new Sprite();
+
+// Step and draw done with prototype because this is faster
+PlayerMissile.prototype.step = function(dt){
+    this.y += this.vy * dt;
+    if(this.y < -this.h){
+        this.board.remove(this);
+    }
+}
+
+
+
+// Enemy function, this again will use prototype for step and draw as it is faster
+var Enemy = function(blueprint, override){
+    
+    this.merge(this.baseParams);
+    this.setup(blueprint.sprite, blueprint);
+    this.merge(override);
+}
+
+// extend sprite
+Enemy.prototype = new Sprite();
+
+// set a property baseParams with some base params for the enemy class
+// used in sprite merge in the constructor for this class creating here prevents
+// the need to create the object for each enemy obj
+Enemy.prototype.baseParams = {A:0, B:0, C:0, D:0,E:0, F:0, G:0, H:0, t:0};
+
+// Step method for enemy
+Enemy.prototype.step = function(dt){
+    this.t = dt;
+    // calculation to move the enemy (use sin to create nice arc movements)
+    this.vx = this.A + this.B * Math.sin(this.C * this.t + this.D);
+    this.vy = this.E + this.F * Math.sin(this.G * this.t + this.H);
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+    //if off the board, remove
+    if(this.y > Game.height || this.x < -this.w || this.x > Game.width){
+        this.board.remove(this);
+    }
+}
--- Alien-invasion/assets/js/game.js
+++ Alien-invasion/assets/js/game.js
@@ -1,28 +1,43 @@
 
+// Define the different types of objects
 
+
+// Define the sprite objects
 var sprites = {
-    ship: {sx:0, sy:0, w:38, h:42, frames:2}
-};
+    ship: {sx:0, sy:0, w:38, h:42, frames:2},
+    missile: {sx:0, sy:30, w:2, h:10, frames: 1},
+    enemy_purple: {sx:37, sy:0, w:42, h:43, frames:1},
+    enemy_bee: {sx:79, sy:0, w:37, h:43, frames:1},
+    enemy_ship: {sx:116, sy:0, w:42, h:43, frames:1},
+    ememy_circle: {sx:158, sy:0, w:32, h:33, frames:1}
+}
+
+var enemies = {
+    basic: {x:100, y:-50, sprite:'enemy_purple', B:100, C:2, E:100}
+}
 
 var startGame = function(){
-    //spriteSheet.draw(Game.ctx, 'ship', 100, 100, 0);
-    
+
     // Add the starfield boards to the game
     Game.setBoard(0, new Starfield(20, 0.4, 100, true));
     Game.setBoard(1, new Starfield(50, 0.6, 100));
     Game.setBoard(2, new Starfield(100, 1.0, 50));
     
     // Add the title board to the game 
-    Game.setBoard(3, new TitleScreen('Gareth Rocks!', 'press fire to begin', playGame));
+    Game.setBoard(3, new TitleScreen('Gareth Rocks!', 'press space to begin', playGame));
 }
 
 // play game function (when fire pressed, callback)
 var playGame = function(){
-    // add the player ship to the board 
-    Game.setBoard(3, new PlayerShip());
+    var board = new GameBoard();
+    board.add(new Enemy(enemies.basic));
+    board.add(new Enemy(enemies.basic, {x:150}));
+    board.add(new PlayerShip());
+    Game.setBoard(3, board);
 }
 
 window.addEventListener('load', function(){
-    Game.initialize('canvasElement', sprites, startGame)   
-});
+    // start the game pass the sprites and the callback arround
+    Game.initialize('canvasElement', sprites, startGame);
+})
 
